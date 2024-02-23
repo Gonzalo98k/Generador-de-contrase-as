d9138fa0d15900e9fbeb73ea4d185812906f5dbb
Gonzalo98k <gonzalo98k@gmail.com>
2024-02-23 00:18:31 -0300

    Se está trabajando en la selección de los checkbox (Mayúsculas, Minúsculas, Números, Simbolos) para la personalización de las contraseñas

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ let contador = document.querySelector(".contador")
 
 childContainer.textContent = "Genere una contraseña"
 
+let arrayDeArrays =[
+    ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "LL", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "ll", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+    [1,2,3,4,5,6,7,8,9],
+
+    ["@", "*", ".", ",", "#", "!", "$", "%", "&", "/", "(", ")", "=", "?", "¿", "¡", "+",]
+]
+
+
+
 const verificarValores = ()=>{
     inputRange.addEventListener("change", ()=>{
         contador.textContent = Math.floor(inputRange.value/10)
@@ -27,12 +39,8 @@ const extraerValores = ()=>{
             
         });
     })
-    
+
 
 }
 
 extraerValores()
-
-setTimeout(() => {
-    console.log(arrayDeIds);
-}, 6000);
