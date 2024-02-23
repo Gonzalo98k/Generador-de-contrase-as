58a1c4047eee6e4e11997e5e8936e7e7ae4bfe89
Gonzalo98k <gonzalo98k@gmail.com>
2024-02-23 15:47:56 -0300

    trabajando sobre el archivo de tipo js creando la función llamada 'comparación de valores' para determinar a través del length de arrayDeIds sobre qué array del array de arrays se debe iterar

--- index.js
+++ index.js
@@ -36,11 +36,68 @@ const extraerValores = ()=>{
             if (e.checked == true) {
                 arrayDeIds.push(e.id)
             }
-            
         });
+
+        comparacionDeValores()
     })
 
 
 }
 
-extraerValores()
+ extraerValores()
+
+ const comparacionDeValores = ()=>{
+    if (arrayDeIds.length == 1) {
+        let value = Math.floor(inputRange.value/10)
+
+        for (let i = 0; i < value; i++) {
+            let random = Math.floor(Math.random()*value)
+
+            console.log(arrayDeArrays[arrayDeIds[0]][random]);
+        }
+
+    }else if(arrayDeIds.length ==  2){
+        let newArr = arrayDeArrays[arrayDeIds[0]].concat(arrayDeArrays[arrayDeIds[1]])
+
+        console.log(newArr);
+
+        let value = newArr.length
+        let value1 = Math.floor(inputRange.value/10)
+
+        for (let i = 0; i < value1; i++) {
+            let random = Math.floor(Math.random()*value)
+
+            console.log(newArr[random]);
+        }
+
+    }else if (arrayDeIds.length == 3){
+        let newArr = arrayDeArrays[arrayDeIds[0]].concat(arrayDeArrays[arrayDeIds[1]])
+
+        let newNewArr = newArr.concat(arrayDeArrays[arrayDeIds[2]])
+
+        let value = newNewArr.length
+        let value1 = Math.floor(inputRange.value/10)
+
+        for (let i = 0; i < value1; i++) {
+            let random = Math.floor(Math.random()*value)
+
+            console.log(newNewArr[random]);
+        }
+
+    }else if(arrayDeIds.length == 4){
+        let newArr = arrayDeArrays[arrayDeIds[0]].concat(arrayDeArrays[arrayDeIds[1]])
+
+        let newNewArr = newArr.concat(arrayDeArrays[arrayDeIds[2]])
+
+        let lastArr = newNewArr.concat(arrayDeArrays[arrayDeIds[3]])
+
+        let value = lastArr.length
+        let value1 = Math.floor(inputRange.value/10)
+
+        for (let i = 0; i < value1; i++) {
+            let random = Math.floor(Math.random()*value)
+
+            console.log(lastArr[random]);
+        }
+    }
+ }
